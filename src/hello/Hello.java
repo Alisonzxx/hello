acd5c28f000f5c69cae68edcfe288bc7f8fcdec2
g14905ga <g14905ga@s30423.ACTSUDA>
2016-04-08 11:23:02 +0900

    Hello Worldです

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -11,6 +11,6 @@ package hello;
  */
 public class Hello {
     public static void main(String[]args){
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
